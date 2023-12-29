ea19d058d93af2506be85387623acfc242453202
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2023-12-29 20:47:02 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("new-message", (message) => {
+    if (message.length) {
+      sendNewMessage(message, socket);
+    }
+  });
+});
+
+const { PORT } = process.env;
+
+server.listen(PORT || 5000, () => {
+  console.log("Server running at " + PORT);
+});
+
+function sendNewMessage(message) {
+  io.emit("new-message", message);
+}
