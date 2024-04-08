93609ad6c1d250524071451f22300a5002656527
THEALAN <105835098+Zack-Dx@users.noreply.github.com>
2024-04-08 08:39:26 +0530

    health response updated.

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ const { ALLOWED_ORIGIN } = Config;
 const app = express();
 
 app.get("/health", (_, res) => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
-    res.sendFile(join(__dirname, "index.html"));
+  // const __dirname = dirname(fileURLToPath(import.meta.url));
+  // res.sendFile(join(__dirname, "index.html"));
+  res.status(200).json({ message: "OK" });
 });
 
 app.use(
-    cors({
-        origin: ALLOWED_ORIGIN,
-    })
+  cors({
+    origin: ALLOWED_ORIGIN,
+  })
 );
 
 export default app;
